96fe7d9b1c2bbbd02d893ffc1fdf3a5ede4a8243
M-Zia-Khatri <kb313892@gmail.com>
2026-06-19 00:01:03 +0500

    chore(db): update default MySQL user password

--- docker/db/init.sql
+++ docker/db/init.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS portfolio;
 
 -- Create the user if it doesn't exist (replace 'your_password' with a real one)
-CREATE USER IF NOT EXISTS 'portfolio_user'@'%' IDENTIFIED BY '1qZIA_plus_MYSQL_MY_USER';
+CREATE USER IF NOT EXISTS 'portfolio_user'@'%' IDENTIFIED BY 'PDBLOCAL';
 
 -- Grant the privileges
 GRANT ALL PRIVILEGES ON portfolio.* TO 'portfolio_user'@'%';
