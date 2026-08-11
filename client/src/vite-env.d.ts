1d028e5c5ed2bf287e94527273df8d17db8ec7df
M-Zia-Khatri <kb313892@gmail.com>
2026-08-12 00:53:16 +0500

    feat(types): add custom module declarations for image handling in Vite

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,2 +1,20 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-imagetools/client" />
+
+declare module "*&as=srcset" {
+  const src: string;
+  export default src;
+}
+
+declare module "*&as=picture" {
+  const src: {
+    img: { src: string; w: number; h: number };
+    sources: Record<string, string>;
+  };
+  export default src;
+}
+
+declare module "*&as=metadata" {
+  const out: Record<string, unknown>;
+  export default out;
+}
